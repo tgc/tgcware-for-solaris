783e0dfc573aa0618fdffa112220723dddbe9717
Tom G. Christensen <tgc@jupiterrise.com>
2013-10-05 13:51:58 +0200

    gmp: update to 5.1.2

--- gmp/build.sh
+++ gmp/build.sh
@@ -6,23 +6,23 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=gmp
-version=5.0.1
+version=5.1.2
 pkgver=1
 source[0]=ftp://ftp.sunet.se/pub/gnu/gmp/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
-#patch[0]=
+patch[0]=gmp-5.1.2-no-c99-trunc.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
-# Global settings
-export LDFLAGS="-L$prefix/lib -R$prefix/lib"
-[ "$_os" = "sunos56" ] && triplet="${arch}-sun-solaris2.6"
-[ "$_os" = "sunos57" ] && triplet="${arch}-sun-solaris2.7"
-configure_args=(--host=$triplet --build=$triplet "${configure_args[@]}" --enable-cxx)
-# otherwise configure tests will fail since they don't respect LDFLAGS :(
-export LD_LIBRARY_PATH="$prefix/lib"
+# Get host triplet
+. ${BUILDPKG_BASE}/gcc/build.sh.gcc.cpu
 
+# Global settings
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export LD_OPTIONS="-R$prefix/lib"
+configure_args=(--host=$gmp_host --build=$gmp_host "${configure_args[@]}" --enable-cxx)
 
 reg prep
 prep()
@@ -48,10 +48,12 @@ install()
     generic_install DESTDIR
     doc AUTHORS COPYING COPYING.LIB NEWS README
 
-    # Compat libraries
+    # Compat library for stuff built against gmp 4.2
     setdir ${prefix}/${_libdir}
     ${__tar} -cf - libgmp.so.3* | (cd ${stagedir}${prefix}/${_libdir}; ${__tar} -xf -)
     compat gmp 4.2.4 1 5
+    #
+    compat gmp 5.0.1 1 1
 }
 
 reg pack
