543aa99c8bb7875fb445b942de41baad7cfc71e0
Tom G. Christensen <tgc@jupiterrise.com>
2013-11-17 21:53:05 +0100

    tk: rebuild

--- tk/build.sh
+++ tk/build.sh
@@ -7,7 +7,7 @@
 # Check the following 4 variables before running the script
 topdir=tk
 version=8.4.19
-pkgver=1
+pkgver=2
 source[0]=${topdir}${version}-src.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -17,7 +17,7 @@ source[0]=${topdir}${version}-src.tar.gz
 
 # Global settings
 export CPPFLAGS="-I$prefix/include"
-export LDFLAGS="-L$prefix/lib -R$prefix/lib"     
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 configure_args=(--prefix=$prefix --mandir=$prefix/$_mandir --disable-symbols --enable-man-symlinks --with-tcl=${prefix}/${_libdir})
 topsrcdir=$topdir$version
 
@@ -50,6 +50,10 @@ install()
     ${__ln} -s wish${majorver} wish
     setdir ${stagedir}${prefix}/${_libdir}
     ${__ln} -s libtk${majorver}.so libtk.so
+    chmod 755 libtk${majorver}.so
+    # Rerun strip
+    custom_install=1
+    generic_install
 
     # Grab headers
     ${__mkdir} -p ${stagedir}${prefix}/${_includedir}/tk-private/{generic,unix}
