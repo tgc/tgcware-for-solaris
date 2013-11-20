6886992c6255f73b3f153c135bc404ce9f13a664
Tom G. Christensen <tgc@jupiterrise.com>
2013-11-20 18:07:07 +0100

    xzutils: update to 5.0.5

--- xzutils/build.sh
+++ xzutils/build.sh
@@ -6,8 +6,8 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=xz
-version=5.0.3
-pkgver=2
+version=5.0.5
+pkgver=1
 source[0]=http://tukaani.org/xz/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -18,12 +18,13 @@ source[0]=http://tukaani.org/xz/$topdir-$version.tar.gz
 # Global settings
 export CPPFLAGS="-I/usr/tgcware/lib"
 export LDFLAGS="-L/usr/tgcware/lib -R/usr/tgcware/lib"
+ac_overrides="gl_cv_posix_shell=/usr/bin/ksh"
 
 reg prep
 prep()
 {
     generic_prep
-    if [ "$_os" = "sunos56" -a "$arch" = "i386" ]; then
+    if [ "$arch" = "i386" ]; then
       # Replacing Wl,-z text with -mimpure-text is a workaround to avoid
       # "ld: fatal: relocations remain against allocatable but non-writable sections"
       # when linking liblzma
@@ -49,6 +50,7 @@ install()
 {
     generic_install DESTDIR
     ${__mv} ${stagedir}${prefix}/${_docdir}/xz ${stagedir}${prefix}/${_vdocdir}
+    compat xzutils 5.0.3 1 1
 }
 
 reg pack
