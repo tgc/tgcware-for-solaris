e0085f0867bd38243b77290f63906cf65479ad6c
Tom G. Christensen <tgc@jupiterrise.com>
2013-11-24 00:23:05 +0100

    gcc42: rename and rebuild
    This updates the build to use the common gcc build
    infrastructure.
    Additionally the package is renamed from gcc424 to gcc42
    to match tgcwarev2.

--- gcc42/build.sh
+++ gcc42/build.sh
@@ -5,50 +5,39 @@
 #
 ###########################################################
 # Check the following 4 variables before running the script
-snapshot=
 topdir=gcc
 version=4.2.4
-pkgver=1
-source[0]=$topdir-$version.tar.bz2
-[ -n "$snapshot" ] && source[0]=$topdir-$version-$snapshot.tar.bz2
+pkgver=2
+source[0]=ftp://ftp.sunet.se/pub/gnu/gcc/releases/$topdir-$version/$topdir-$version.tar.bz2
 ## If there are no patches, simply comment this
-#patch[0]=
+patch[0]=gcc-4.2.4-new-gas.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
+# Common settings for gcc
+. ${BUILDPKG_BASE}/gcc/build.sh.gcc.common
+
 # Global settings
-[ -n "$snapshot" ] && topsrcdir=gcc-$version-$snapshot
-lprefix=$prefix
-prefix=/usr/tgcware/$topdir-$version
-__configure="../$topsrcdir/configure"
-make_build_target=bootstrap
 
-# Define abbreviated version number (for pkgdef)
-abbrev_ver=$(echo $version | ${__tr} -d '.')
-
-# Configure args
-configure_args=(--prefix=$prefix --with-local-prefix=$prefix --with-libiconv-prefix=$lprefix --with-gmp=$lprefix --with-mpfr=$lprefix --disable-nls --enable-shared --enable-threads=posix95)
-configure_args+=(--enable-languages=c,ada,c++,fortran,objc,obj-c++)
-objdir=all_native
-# platform/arch specific options
-[ "$_os" = "sunos56" -a "$arch" = "i386" ] && configure_args+=(--with-gnu-as --with-as=$lprefix/bin/gas)
-[ "$arch" = "sparc" ] && vendor="sun" || vendor="pc"
-
-export CONFIG_SHELL=/bin/ksh
+# This compiler is bootstrapped with gcc 4.1.2
+export PATH=/usr/tgcware/gcc41/bin:$PATH
 
 reg prep
 prep()
 {
     generic_prep
+    setdir source
+    # Set bugurl and vendor version
+    ${__gsed} -i "s|URL:[^>]*|URL:$gccbugurl|" gcc/version.c
+    ${__gsed} -i "/VERSUFFIX/ s/\"\"/\" ($gccpkgversion)\"/" gcc/version.c
 }
 
 reg build
 build()
 {
-    setdir source
-    ${__mkdir} -p ../$objdir
-    echo "$__configure $configure_args"
+    setup_tools
+    ${__mkdir} -p ${srcdir}/$objdir
     generic_build ../$objdir
 }
 
@@ -62,27 +51,15 @@ install()
     generic_install
     ${__find} ${stagedir} -name '*.la' -print | ${__xargs} ${__rm} -f
 
-    # Prepare for split lib packages
-    ${__mkdir} -p ${stagedir}${lprefix}/${_libdir}
-    setdir ${stagedir}${prefix}/${_libdir}
-    ${__tar} -cf - libgcc_s.so.1 libstdc++.so.6* libgfortran.so.2* libobjc.so.2* libgomp.so.1* |
-	(cd ${stagedir}${lprefix}/${_libdir}; ${__tar} -xvBpf -)
+    # Rearrange libraries
+    redo_libs
+
+    # Turn all the hardlinks in bin into symlinks
+    redo_bin
 
     # Place share/docs in the regular location
     prefix=$topinstalldir
     doc COPYING* BUGS FAQ MAINTAINERS NEWS
-
-    # Setup compat files
-    for pkg in libgcc_s1 libstdc++6
-    do
-        ${__rm} -f $metadir/compver.$pkg
-        compat $pkg 3.4.6 1 5
-        compat $pkg 4.0.4 1 2
-        compat $pkg 4.1.2 1 2
-        compat $pkg 4.2.3 1 2
-    done
-    compat libobjc2 4.2.3 1 2
-    compat libgfortran2 4.2.3 1 2
 }
 
 reg check
@@ -96,7 +73,7 @@ check()
 reg pack
 pack()
 {
-    iprefix=$topdir-$version
+    iprefix=${topdir}${abbrev_majorminor}
     generic_pack
 }
 
