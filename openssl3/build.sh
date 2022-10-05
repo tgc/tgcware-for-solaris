987ab97e4e7740bf9b1049a08b281a60b66a1300
Tom G. Christensen <tgc@jupiterrise.com>
2022-10-05 14:06:17 +0200

    openssl3: update to 3.0.5

--- openssl3/build.sh
+++ openssl3/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=openssl
-version=3.0.3
+version=3.0.5
 pkgver=1
 source[0]=https://openssl.org/source/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
@@ -84,6 +84,7 @@ install()
 
     # Compatible with previous releases
     compat openssl 3.0.2 1 1
+    compat openssl 3.0.3 1 1
 }
 
 reg pack
