c6fabe37d92f91bed5136f1281f9022850de5479
Tom G. Christensen <tgc@jupiterrise.com>
2022-11-04 19:48:48 +0100

    curl: update to 7.86.0

--- curl/build.sh
+++ curl/build.sh
@@ -6,11 +6,11 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=7.83.1
+version=7.86.0
 pkgver=1
 source[0]=http://curl.se/download/$topdir-$version.tar.bz2
 # https://curl.se/docs/caextract.html
-certdate=2022-04-26
+certdate=2022-10-11
 source[1]=https://curl.se/ca/cacert-$certdate.pem
 # If there are no patches, simply comment this
 patch[0]=curl-7.68.0-socklen_t.patch
@@ -27,7 +27,7 @@ export PKG_CONFIG=pkgconf
 
 # Prefer the X/Open feature set to get utimes() defined
 export CC="gcc -D__EXTENSIONS__ -D_XOPEN_SOURCE -D_XOPEN_SOURCE_EXTENDED=1"
-configure_args+=(--enable-static=no --with-openssl --enable-http --enable-ftp --enable-file --disable-ldap --enable-manual --enable-cookies --enable-crypto --with-libidn2 --with-libssh2 --with-nghttp2 --with-ca-bundle=${prefix}/${_sysconfdir}/curl-ca-bundle.pem --disable-threaded-resolver)
+configure_args+=(--enable-static=no --with-openssl --enable-http --enable-ftp --enable-file --disable-ldap --enable-manual --enable-cookies --with-libidn2 --with-libssh2 --with-nghttp2 --with-ca-bundle=${prefix}/${_sysconfdir}/curl-ca-bundle.pem --disable-threaded-resolver)
 
 reg prep
 prep()
@@ -67,7 +67,7 @@ install()
 	7.21.6 7.33.0 7.36.0 7.37.1 7.38.1 7.41.0 7.42.0 7.42.1 7.44.0 \
 	7.45.0 7.46.0 7.48.0 7.49.0 7.50.0 7.50.3 7.51.0 7.52.1 7.55.1 \
 	7.59.0 7.61.1 7.64.0 7.64.1 7.69.1 7.72.0 7.73.0 7.75.0 7.76.0 \
-	7.76.1 7.79.1 7.82.0
+	7.76.1 7.79.1 7.82.0 7.83.1
     do
 	compat curl $release 1 1
     done
