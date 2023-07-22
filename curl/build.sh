9cca18790875c5e9448f6883fc1e57418850a6ee
Tom G. Christensen <tgc@jupiterrise.com>
2023-07-22 11:15:56 +0200

    curl: update to 8.2.0

--- curl/build.sh
+++ curl/build.sh
@@ -6,16 +6,18 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=7.87.0
+version=8.2.0
 pkgver=1
 source[0]=http://curl.se/download/$topdir-$version.tar.bz2
 # https://curl.se/docs/caextract.html
-certdate=2023-01-10
+certdate=2023-05-30
 source[1]=https://curl.se/ca/cacert-$certdate.pem
 # If there are no patches, simply comment this
 patch[0]=curl-7.68.0-socklen_t.patch
 # OpenSSH 8.8 disabled sha1 rsa out of the box
 patch[1]=curl-7.82.0-modern-openssh.patch
+# No stdint.h
+patch[2]=curl-8.2.0-no-stdint-h.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -67,7 +69,7 @@ install()
 	7.21.6 7.33.0 7.36.0 7.37.1 7.38.1 7.41.0 7.42.0 7.42.1 7.44.0 \
 	7.45.0 7.46.0 7.48.0 7.49.0 7.50.0 7.50.3 7.51.0 7.52.1 7.55.1 \
 	7.59.0 7.61.1 7.64.0 7.64.1 7.69.1 7.72.0 7.73.0 7.75.0 7.76.0 \
-	7.76.1 7.79.1 7.82.0 7.83.1
+	7.76.1 7.79.1 7.82.0 7.83.1 7.87.0
     do
 	compat curl $release 1 1
     done
