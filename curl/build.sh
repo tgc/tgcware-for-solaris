23e64a72fb9f4220807087924e60139b5c46c2b5
Tom G. Christensen <tgc@jupiterrise.com>
2023-10-11 14:32:09 +0200

    curl: update to 8.4.0

--- curl/build.sh
+++ curl/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=curl
-version=8.3.0
+version=8.4.0
 pkgver=1
 source[0]=http://curl.se/download/$topdir-$version.tar.xz
 # https://curl.se/docs/caextract.html
@@ -70,7 +70,7 @@ install()
 	7.21.6 7.33.0 7.36.0 7.37.1 7.38.1 7.41.0 7.42.0 7.42.1 7.44.0 \
 	7.45.0 7.46.0 7.48.0 7.49.0 7.50.0 7.50.3 7.51.0 7.52.1 7.55.1 \
 	7.59.0 7.61.1 7.64.0 7.64.1 7.69.1 7.72.0 7.73.0 7.75.0 7.76.0 \
-	7.76.1 7.79.1 7.82.0 7.83.1 7.87.0 8.2.0
+	7.76.1 7.79.1 7.82.0 7.83.1 7.87.0 8.2.0 8.3.0
     do
 	compat curl $release 1 1
     done
